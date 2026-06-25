7b154111e2481c9f762a40dbcd96f718b9a03bb4
Tomasz Mondrzycki <tomasz.mondrzycki@capgemini.com>
2026-06-25 16:21:56 +0200

    build complete

---
+++ app/customer/dist/Component-dbg.js
@@ -0,0 +1,14 @@
+sap.ui.define(["sap/fe/core/AppComponent"], function (BaseComponent) {
+  "use strict";
+
+  /**
+   * @namespace btp.starterkit.customer
+   */
+  const Component = BaseComponent.extend("btp.starterkit.customer.Component", {
+    metadata: {
+      manifest: "json"
+    }
+  });
+  return Component;
+});
+//# sourceMappingURL=Component-dbg.js.map
---
+++ app/customer/dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(t){"use strict";const e=t.extend("btp.starterkit.customer.Component",{metadata:{manifest:"json"}});return e});
+//# sourceMappingURL=Component.js.map
